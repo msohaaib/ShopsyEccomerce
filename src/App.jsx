e8cc6f51d252bfab2e1bf81af1975434960c74ac
msohaaib <msohaib9685055@gmail.com>
2024-08-18 19:34:50 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
 
   return (
     <>
-      <h1 className='bg-red-400 text-3xl underline'>HEllo</h1>
+      <Navbar />
           </>
   )
 }
